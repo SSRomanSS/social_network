3b46a4051b3d22b2a1a5df4226a2e25732cc5c31
Roman Shkapiak <r.shkapiak@gmail.com>
2020-07-11 11:53:45 +0300

    Add model for likes based on ContentType

--- posts/models.py
+++ posts/models.py
@@ -1,5 +1,16 @@
 from django.contrib.auth import get_user_model
 from django.db import models
+from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
+from django.contrib.contenttypes.models import ContentType
+
+
+class Like(models.Model):
+    user = models.ForeignKey(get_user_model(),
+                             related_name='likes',
+                             on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
 
 
 class Post(models.Model):
@@ -9,6 +20,7 @@ class Post(models.Model):
     )
     body = models.TextField()
     date_published = models.DateTimeField(auto_now_add=True)
+    likes = GenericRelation(Like)
 
     def __str__(self):
         return f'"{self.body}" posted by {self.author}'
@@ -17,3 +29,7 @@ class Post(models.Model):
         verbose_name = 'Post'
         verbose_name_plural = 'Posts'
         ordering = ['-date_published']
+
+    @property
+    def likes_sum(self):
+        return self.likes.count()
